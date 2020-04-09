57b13b772d26f1105e630958220f7701df10063b
pierreamir123 <pa83954@hotmail.com>
2020-04-09 16:38:44 +0200

    pierre1

--- p/CMP102_Porj_code_Spring2020/Restaurant/Rest/Order.cpp
+++ p/CMP102_Porj_code_Spring2020/Restaurant/Rest/Order.cpp
@@ -1,21 +1,30 @@
 #include "Order.h"
 
-Order::Order(int id, ORD_TYPE r_Type)
+Order :: Order(int id, ORD_TYPE r_Type, int arrtime, int size, double money)      // --> ma3mltesh intialize le finish time w distance 3alshan law tel3o garbage a3raf eni ma3mltlhomsh set
 {
-	ID = (id>0&&id<1000)?id:0;	//1<ID<999
+	ID = ( id > 0 && id < 1000 ) ? id : 0;  	//1<ID<999    
 	type = r_Type;
 	status = WAIT;
+	ArrTime = arrtime;
+	Size = size;
+	totalMoney = money;
+	WaitTime = 0;
+	ServTime = 0;
 }
 
 Order::~Order()
 {
 }
 
-int Order::GetID()
+int Order::GetID() const       
 {
 	return ID;
 }
 
+void Order :: setType ( ORD_TYPE t )
+{
+	type = t;
+}
 
 ORD_TYPE Order::GetType() const
 {
@@ -25,7 +34,7 @@ ORD_TYPE Order::GetType() const
 
 void Order::SetDistance(int d)
 {
-	Distance = d>0?d:0;
+	Distance = d > 0 ? d : 0;
 }
 
 int Order::GetDistance() const
@@ -44,3 +53,52 @@ ORD_STATUS Order::getStatus() const
 	return status;
 }
 
+double Order ::  getTotalMoney() const
+{
+	return totalMoney;
+}
+
+void Order :: incrementTotalMoney( double money )
+{
+	totalMoney += money;
+}
+
+void Order :: incrementWaitTime ()
+{
+	WaitTime++;
+}
+
+void Order :: incrementServTime ()
+{
+	ServTime++;
+}
+
+void Order :: updateFinishTime ()
+{
+	FinishTime = ArrTime + WaitTime + ServTime;
+}
+
+int Order :: getArrTime () const
+{
+	return ArrTime;
+}
+
+int Order :: getWaitTime () const
+{
+	return WaitTime;
+}
+
+int Order :: getServTime () const
+{
+	return ServTime;
+}
+
+int Order :: getFinishTime () const
+{
+	return FinishTime;
+}
+
+int Order :: getSize () const
+{
+	return Size;
+}
--- p/CMP102_Porj_code_Spring2020/Restaurant/Rest/Order.h
+++ p/CMP102_Porj_code_Spring2020/Restaurant/Rest/Order.h
@@ -7,26 +7,29 @@ class Order
 {
 
 protected:
-	int ID;         //Each order has a unique ID (from 1 --> 999 )
+
+	int ID;              //Each order has a unique ID (from 1 --> 999 )
 	ORD_TYPE type;		//order type: Normal, vegan, VIP
 	ORD_STATUS status;	//waiting, in-service, done
 	int Distance;	//The distance (in meters) between the order location and the resturant 
 	                
 	double totalMoney;	//Total order money
 
-	int ArrTime, ServTime, FinishTime;	//arrival, service start, and finish times
+	int ArrTime, WaitTime, ServTime, FinishTime;	//arrival, service start, and finish times
 	
+	int Size;
+
+	//////////////////// -------->>>>> auto-promotion ??
 	
-	//
-	// TODO: Add More Data Members As Needed
-	//
 
 public:
-	Order(int ID, ORD_TYPE r_Type);
+
+	Order(int ID, ORD_TYPE r_Type, int arrtime, int size, double money); 
 	virtual ~Order();
 
-	int GetID();
+	int GetID() const;    
 
+	void setType ( ORD_TYPE t ); // --> 3alshan el promotion event
 	ORD_TYPE GetType() const;
 
 	void SetDistance(int d);
@@ -34,10 +37,23 @@ public:
 
 	void setStatus(ORD_STATUS s);
 	ORD_STATUS getStatus() const;
+
+	double getTotalMoney() const;                // --> 3alshan lama arateb el VIPs fel queue beta3hom
+	void incrementTotalMoney( double money );    // --> 3alshan el promotion event
+
+	int getArrTime () const;      // --> 3alshan atala3ha fel O/P file
+
+	void incrementWaitTime ();   // --> 3alshan a3ed el timesteps elli fedel feh el order fel waiting queues
+	int getWaitTime () const;    // --> 3alshan atala3ha fel O/P file w asta5demha fel auto-promotion
+
+	void incrementServTime ();   // --> 3alshan a3ed el timesteps elli fedel feh el order fel in-service list
+	int getServTime () const;    // --> 3alshan atala3ha fel O/P file
+
+	void updateFinishTime ();    // --> 3alshan y-update el finish time ba3d ma atala3 el order men el in-service list lel finished list
+	int getFinishTime () const;  // --> 3alshan atala3ha fel O/P file
+
+	int getSize () const;     // --> 3alshan lama arateb el VIPs fel queue beta3hom
 	
-	//
-	// TODO: Add More Member Functions As Needed
-	//
 
 };
 
